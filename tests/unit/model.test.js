38a42d2eed1f7a96c84d0e2e623c166acd99de0c
NguyenThangLoi <ntl10a6@gmail.com>
2025-11-02 15:15:34 +0700

    Test: Implement unit tests for TodoService

--- tests/unit/model.test.js
+++ tests/unit/model.test.js
@@ -14,6 +14,12 @@ describe('TodoService Unit Tests', () => {
         // TODO: Call the addTodo method with some text.
         // Then, assert that the service's todos array has a length of 1.
         // Assert that the text of the first todo matches the input text.
+        text = 'My new todo';
+
+        service.addTodo('My new todo');
+
+        expect(service.todos.length).toBe(1);
+        expect(service.todos[0].text).toMatch(text);
     });
 
     test('should toggle the completed state of a todo', () => {
@@ -21,16 +27,31 @@ describe('TodoService Unit Tests', () => {
         // Then, get its ID and call the toggleTodoComplete method.
         // Assert that the 'completed' property of that todo is now true.
         // Call toggleTodoComplete again and assert that it's false.
+         service.addTodo('My new todo 1');
+        id = service.getTodos().find(t => t.text === "My new todo 1").id;
+        idx = service.getTodos().findIndex(t => t.text === "My new todo 1");
+
+        service.toggleTodoComplete(id);
+        expect(service.todos[idx].completed).toBe(true);
+        service.toggleTodoComplete(id);
+        expect(service.todos[idx].completed).toBe(false);
     });
 
     test('should remove a todo', () => {
         // TODO: Add a todo.
         // Get its ID and call the removeTodo method.
         // Assert that the service's todos array is now empty (length of 0).
+        service.addTodo('My new todo 2');
+        id = service.getTodos().find(t => t.text === "My new todo 2").id;
+        service.removeTodo(id);
+        expect(service.todos.length).toBe(0);
     });
 
     test('should not add a todo if text is empty', () => {
         // TODO: Call addTodo with an empty string.
         // Assert that the todos array still has a length of 0.
+        let text = "";
+        service.addTodo(text);
+        expect(service.todos.length).toBe(0);
     });
 });
